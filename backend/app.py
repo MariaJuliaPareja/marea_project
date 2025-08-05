4d4834fb4705168850b9c34c3d6b3ad91cf17b5d
Maria Julia Pareja Abarca <parejamariajulia@gmail.com>
2025-08-05 13:29:30 -0500

    Imports for the backend

---
+++ backend/app.py
@@ -0,0 +1,9 @@
+#Imports
+from flask import Flask, request, jsonify, render_template_string
+from flask_cors import CORS
+import sqlite3
+import json
+import time
+from datetime import datetime
+import os
+

fb0507befeb4971c58e644a05aad29d42c828f46
Maria Julia Pareja Abarca <parejamariajulia@gmail.com>
2025-08-05 19:26:00 -0500

    Starting the Flask application and main components

--- backend/app.py
+++ backend/app.py
@@ -9,4 +9,13 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 #Creating function for the main app
 def create_app():
-   
+    # Flask application
+    app = Flask(__name__)
+    CORS(app)
+    # Configuration
+    app.config['DATABASE_PATH'] = 'data/marea_data.db'
+    # Initialize components
+    init_components(app)
+    # Register modular routes
+    register_blueprints(app)
+    return app

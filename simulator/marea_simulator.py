1a61aa79a900ed1ff8fc155a89a232a600b0a534
Maria Julia Pareja Abarca <parejamariajulia@gmail.com>
2025-08-05 19:44:31 -0500

    Creating a Simulation for the sensors

---
+++ simulator/marea_simulator.py
@@ -0,0 +1,12 @@
+#Imports
+import json
+import time
+import random
+import requests
+import numpy as np
+from datetime import datetime
+
+#Class which will work as a simulation of the sensors 
+class MAREASimulator:
+    if __name__ == "__main__":
+        main()
